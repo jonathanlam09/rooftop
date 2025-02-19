b09a55813c2c049f45f1d87af739b71a9c28d9cf
Jon <jonathanlam09@gmail.com>
2025-02-19 16:32:01 +0800

    Branch name: master Description: - Change localhost url to live

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,14 @@ import * as helper from './helper';
 
 const apiHeader = { 'Content-Type': 'multipart/form-data' };
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://rooftop-energy-api.netlify.app/api';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Content-Type'] = apiHeader['Content-Type'];
 
 export const config = {
     properties: {
         baseURL: window.location.origin + '/',
-        assetPath: 'http://localhost:3000/',
+        assetPath: 'https://rooftop-energy-api.netlify.app/',
         apiHeader: apiHeader,
     },
     methods: helper,
